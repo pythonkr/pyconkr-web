f88fc0cd153683d5d3b906b1399bb42c1af18b39
Hyeok Min Kwon <khmzxc@naver.com>
2024-03-31 16:55:22 +0900

    Add page (WIP)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,16 @@
+import Nav from "components/Nav";
+import Home from "pages/Home";
+import SponsorList from "pages/Sponsor";
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/sponsor" />
+        <Route path="/" element={<Home />} />
+        <Route path="/sponsor" element={<SponsorList />} />
         <Route path="/404" />
         <Route path="*" element={<Navigate replace to={"/404"} />} />
       </Routes>
