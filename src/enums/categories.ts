31b4e3514b0032b7692f46597bc7b22fab74d428
Hyeok Min Kwon <khmzxc@naver.com>
2024-04-05 23:55:14 +0900

    Edit category enum

--- src/enums/categories.ts
+++ src/enums/categories.ts
@@ -1,7 +1,7 @@
 import { Enum, EnumItem, Enum_isCode } from ".";
 
 /** 세션 카테고리 */
-export type Category = EnumItem<CategoryCode>;
+export type Category = EnumItem<CategoryCode> & { color: `#${string | number}` };
 
 const codes = ["web", "social"] as const; // TODO
 
@@ -18,14 +18,17 @@ const Categories: Enum<Category, CategoryCode> = {
   web: {
     code: "web",
     name: "웹",
+    color: "#291819",
   },
   social: {
     code: "social",
     name: "소셜",
+    color: "#291819",
   },
   unknown: {
     code: "unknown",
     name: "알 수 없음",
+    color: "#000000",
   },
 };
 
