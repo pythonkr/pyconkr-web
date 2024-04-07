ad23201104bfcff3ef1ff1cfb2576e96e1d04fd0
Hyeok Min Kwon <khmzxc@naver.com>
2024-04-07 21:13:01 +0900

    Edit API mock

--- src/api/sponsor.ts
+++ src/api/sponsor.ts
@@ -5,6 +5,19 @@ import { getErrorMessage } from "api";
 
 export function listSponsors(): Promise<Sponsor[]> {
   return new Promise((resolve, reject) => {
+    resolve([
+      {
+        id: "1",
+        name: "후원사1",
+      },
+      {
+        id: "2",
+        name: "후원사2",
+      },
+    ]);
+    return;
+
+    // eslint-disable-next-line no-unreachable
     axios
       .get<APISponsor[]>("/sponsor")
       .then((response) => {
