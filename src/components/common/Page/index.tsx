e94029fd68fe66a189b84be7c53a284b33329f4a
Hyeok Min Kwon <khmzxc@naver.com>
2024-04-14 21:01:04 +0900

    Add translation in page component

--- src/components/common/Page/index.tsx
+++ src/components/common/Page/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.scss";
 import { Helmet } from "react-helmet";
+import useTranslation from "utils/hooks/useTranslation";
 
 const Page = ({ children, title }: React.HTMLAttributes<HTMLDivElement> & { title?: string }) => {
+  const t = useTranslation();
+
   return (
     <div className="cm-page">
-      <Helmet title={!title ? "파이콘 한국 2024" : `파이콘 한국 2024 | ${title}`} />
+      <Helmet title={!title ? `${t("파이콘 한국")} 2024` : `${t("파이콘 한국")} 2024 | ${title}`} />
       {children}
     </div>
   );
