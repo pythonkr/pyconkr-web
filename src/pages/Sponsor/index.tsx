cd9c14f096ed270fb3a2aa4b8a8d8690ea993f0d
Hyeok Min Kwon <khmzxc@naver.com>
2024-04-14 15:57:31 +0900

    Remove unused

--- src/pages/Sponsor/index.tsx
+++
@@ -1,8 +0,0 @@
-import Page from "components/common/Page";
-import React from "react";
-
-const SponsorList = () => {
-  return <Page>후원사 목록</Page>;
-};
-
-export default SponsorList;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,6 @@ import Footer from "components/Footer";
 import Nav from "components/Nav";
 import NotFound from "pages/NotFound";
 import Home from "pages/Home";
-import SponsorList from "pages/Sponsor";
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "pages/Login";
@@ -15,7 +14,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/sponsor" element={<SponsorList />} />
         <Route path="/ticket/buy" element={<BuyTicket />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate replace to={"/404"} />} />
