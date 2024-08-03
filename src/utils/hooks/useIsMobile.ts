36ea431332aef8020359074b21ff470b9c7abc33
MU-Software <musoftware@daum.net>
2024-08-03 22:54:04 +0900

    발생 가능한 퍼포먼스 이슈를 줄임

--- src/utils/hooks/useIsMobile.ts
+++ src/utils/hooks/useIsMobile.ts
@@ -2,27 +2,19 @@ import { useCallback, useEffect, useState } from "react"
 
 const useMediaQuery = (width: number) => {
   const [targetReached, setTargetReached] = useState(false);
-  const updateTarget = useCallback((e: any) => {
-    if (e.matches) {
-      setTargetReached(true);
-    } else {
-      setTargetReached(false);
-    }
-  }, []);
+
+  const updateTarget = useCallback((e: any) => setTargetReached(e.matches ? true : false), []);
   useEffect(() => {
     const media = window.matchMedia(`(max-width: ${width}px)`);
-    media.addListener(updateTarget);
+    media.addEventListener('change', updateTarget);
     // Check on mount (callback is not called until a change occurs)
-    if (media.matches) {
-      setTargetReached(true);
-    }
-    return () => media.removeListener(updateTarget);
-  }, []);
+    if (media.matches) setTargetReached(true);
+    return () => media.removeEventListener('change', updateTarget);
+  }, [updateTarget, width]);
+
   return targetReached;
 };
 
-const useIsMobile = () => {
-  return useMediaQuery(809);
-};
+const useIsMobile = () => useMediaQuery(809);
 
 export default useIsMobile;
