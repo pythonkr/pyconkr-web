645e6201b18926b93cdbd2655ff18edd6abd43cf
Hyeok Min Kwon <khmzxc@naver.com>
2024-09-24 22:01:11 +0900

    Remove logging

--- src/api/sponsor.ts
+++ src/api/sponsor.ts
@@ -1,29 +1,39 @@
-import { getErrorMessage } from "api"
-import instance from "lib/axios"
-import { APISponsor, APISponsorBenefit, APISponsorLevel, APISponsorLevelWithSponsor } from "models/api/sponsor"
-import { Sponsor, SponsorBenefit, SponsorLevel, SponsorLevelWithSponsor } from "models/sponsor"
-
+import { getErrorMessage } from "api";
+import instance from "lib/axios";
+import {
+  APISponsor,
+  APISponsorBenefit,
+  APISponsorLevel,
+  APISponsorLevelWithSponsor,
+} from "models/api/sponsor";
+import { Sponsor, SponsorBenefit, SponsorLevel, SponsorLevelWithSponsor } from "models/sponsor";
 
 export function detailSponsor(id: string): Promise<Sponsor> {
   return new Promise((resolve, reject) => {
-    instance.get<APISponsor>(`/2024/sponsors/list/${id}/`).then(response => {
-      resolve(Sponsor.fromAPI(response.data));
-    }).catch(error => {
-      console.error(error);
-      reject(getErrorMessage(error));
-    })
+    instance
+      .get<APISponsor>(`/2024/sponsors/list/${id}/`)
+      .then((response) => {
+        resolve(Sponsor.fromAPI(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(getErrorMessage(error));
+      });
     return;
-  })
+  });
 }
 
 export function listSponsorLevels(): Promise<SponsorLevel[]> {
   return new Promise((resolve, reject) => {
-    instance.get<APISponsorLevel[]>("/2024/sponsors/levels").then((response) => {
-      resolve(SponsorLevel.fromAPIs(response.data));
-    }).catch((error) => {
-      console.error(error);
-      reject(getErrorMessage(error));
-    })
+    instance
+      .get<APISponsorLevel[]>("/2024/sponsors/levels")
+      .then((response) => {
+        resolve(SponsorLevel.fromAPIs(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(getErrorMessage(error));
+      });
     return;
   });
 }
@@ -46,7 +56,6 @@ export function listSponsorLevelWithSponsor(): Promise<SponsorLevelWithSponsor[]
     instance
       .get<APISponsorLevelWithSponsor[]>("/2024/sponsors/levels/with-sponsor/")
       .then((response) => {
-        console.log("debug", response);
         resolve(SponsorLevelWithSponsor.fromAPIs(response.data));
       })
       .catch((error) => {
@@ -58,11 +67,14 @@ export function listSponsorLevelWithSponsor(): Promise<SponsorLevelWithSponsor[]
 
 export function listSponsorBenefits(): Promise<SponsorBenefit[]> {
   return new Promise((resolve, reject) => {
-    instance.get<APISponsorBenefit[]>("/2024/sponsors/benefits/").then(response => {
-      resolve(SponsorBenefit.fromAPIs(response.data));
-    }).catch(error => {
-      console.error(error);
-      reject(getErrorMessage(error));
-    })
-  })
+    instance
+      .get<APISponsorBenefit[]>("/2024/sponsors/benefits/")
+      .then((response) => {
+        resolve(SponsorBenefit.fromAPIs(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(getErrorMessage(error));
+      });
+  });
 }
