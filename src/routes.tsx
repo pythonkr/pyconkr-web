b736b4e92dbd6c348f45d52d5a8bbbb2cf85e77c
MU-Software <musoftware@daum.net>
2024-10-06 00:26:48 +0900

    장소 안내 페이지 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import React from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
-import Footer from "components/Footer";
-import Nav from "components/Nav";
-import { DialogCollection } from "dialogs";
-import Coc from "pages/About/coc";
-import Pyconkr2024 from "pages/About/pyconkr2024";
-import Cfp from "pages/Contribution/cfp";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import PrivacyPolicy from "pages/PrivacyPolicy";
-import { SessionDetailPage } from "pages/Session/detail";
-import { SessionListPage } from "pages/Session/list";
-import { SessionTimeTablePage } from "pages/Session/timetable";
-import SponsorPage from "pages/Sponsor";
-import TermsOfService from "pages/TermsOfService";
-import Health from "./pages/About/health";
-import SponsorDetailPage from "pages/Sponsor/SponsorDetail";
-import PatronList from "pages/Sponsor/patron";
+import Footer from "components/Footer"
+import Nav from "components/Nav"
+import { DialogCollection } from "dialogs"
+import Coc from "pages/About/coc"
+import { HowToGetThere } from "pages/About/howToGetThere"
+import Pyconkr2024 from "pages/About/pyconkr2024"
+import Cfp from "pages/Contribution/cfp"
+import Home from "pages/Home"
+import NotFound from "pages/NotFound"
+import PrivacyPolicy from "pages/PrivacyPolicy"
+import { SessionDetailPage } from "pages/Session/detail"
+import { SessionListPage } from "pages/Session/list"
+import { SessionTimeTablePage } from "pages/Session/timetable"
+import SponsorPage from "pages/Sponsor"
+import SponsorDetailPage from "pages/Sponsor/SponsorDetail"
+import PatronList from "pages/Sponsor/patron"
+import TermsOfService from "pages/TermsOfService"
+import Health from "./pages/About/health"
 
 const Router = () => {
   return (
@@ -28,6 +29,7 @@ const Router = () => {
         <Route path="/about/pyconkr2024" element={<Pyconkr2024 />} />
         <Route path="/about/coc" element={<Coc />} />
         <Route path="/about/health" element={<Health />} />
+        <Route path="/about/place" element={<HowToGetThere />} />
         <Route path="/sponsoring/sponsor/prospectus" element={<SponsorPage />} />
         <Route path="/sponsoring/sponsor/:id" element={<SponsorDetailPage />} />
         <Route path="/sponsoring/patron" element={<PatronList />} />
